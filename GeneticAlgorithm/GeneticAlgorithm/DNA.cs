79efbc360dc2d475af3f3b7acb83d674fe4ae8a4
kov <venu.k@clovetech.com>
2020-05-02 12:49:28 +0530

    #1-first commit

---
+++ GeneticAlgorithm/GeneticAlgorithm/DNA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    public class DNA<T>
+    {
+        public T[] Genes { get; set; }
+
+        public float Fitness { get; set; }
+        private Random _random;
+        private Func<T> getRandomGene;
+
+        public DNA(int size, Random random, Func<T> getRandomGene, bool showInitGenes = true)
+        {
+            Genes = new T[size];
+            this._random = random;
+            this.getRandomGene = getRandomGene;
+
+            if (showInitGenes)
+            {
+                for (int i = 0; i < Genes.Length; i++)
+                {
+                    Genes[i] = getRandomGene();
+                }
+            }
+        }
+        public float CalculateFitness()
+        {
+            return 0;
+        }
+        public DNA<T> CrossOver(DNA<T> OtherParent)
+        {
+            DNA<T> child = new DNA<T>(Genes.Length, _random, getRandomGene, showInitGenes: false);
+            Random random = new Random();
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                child.Genes[i] = random.NextDouble() < 0.5 ? Genes[i] : OtherParent.Genes[i];
+
+            }
+            return child;
+        }
+        public void Mutate(float mutationRate)
+        {
+            for (int i = 0; i < Genes.Length; i++)
+            {
+                if (_random.NextDouble() < mutationRate)
+                {
+                    Genes[i] = getRandomGene();
+                }
+            }
+        }
+    }
+}
---
+++ GeneticAlgorithm/GeneticAlgorithm/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneticAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+    }
+}
